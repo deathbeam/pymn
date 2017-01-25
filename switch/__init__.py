88871224a5916acfe04991642ec39337fdb4df66
Tomas Slusny <tomas.slusny@pantheon.tech>
2017-01-25 13:25:18 +0100

    Fix
    Signed-off-by: Tomas Slusny <tomas.slusny@pantheon.tech>

--- switch/__init__.py
+++ switch/__init__.py
@@ -1,6 +1,6 @@
 # Mininet defaults
 from mininet.node import OVSSwitch
 from mininet.node import UserSwitch
-from mininet.node import OVSLegacyKernelSwitch
+# from mininet.node import OVSLegacyKernelSwitch
 
 # Custom switches
